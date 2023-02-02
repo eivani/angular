8bec48dc0cbfbf53380e3d9208921aa9b25212e9
mozhdeh.eiv <112771674+eivani@users.noreply.github.com>
2023-02-02 17:42:01 +0330

    Added grid DevExtreme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { HomeComponent } from './features/home/home.component';
 const routes: Routes = [
   {
     path: 'home', component: HomeComponent, children: [
-      { path: 'crm-system', loadChildren: () => import('./features/crm-system/crm-system.module').then(m => m.CrmSystemModule) },
       { path: 'chart', loadChildren: () => import('./features/chart/chart.module').then(m => m.ChartModule) }
     ]
   },
